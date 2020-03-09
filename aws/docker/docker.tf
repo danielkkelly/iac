e5bbb34cf6a805e3e6f06fbfbccdd70057915cd1
Dan Kelly <dan@boxcarapp.com>
2020-03-08 21:09:04 -0400

    Adds the start of a docker instance setup

---
+++ aws/docker/docker.tf
@@ -0,0 +1,67 @@
+provider "aws" {
+  region = var.region
+}
+
+data "aws_vpc" "vpc" {
+  tags = {
+    Name        = "platform-vpc"
+  }
+}
+
+data "aws_subnet" "subnet_pri_1" {
+  vpc_id = data.aws_vpc.vpc.id
+
+  tags = {
+    Type        = "private"
+    Number      = "1"
+  }
+}
+
+data "aws_security_group" "bastion_sg" {
+  tags = {
+    Name 	= "platform-bastion"
+  }
+}
+ 
+resource "aws_security_group" "docker_sg" {
+ 
+  vpc_id        = data.aws_vpc.vpc.id
+  name          = "platform-docker"
+  description   = "SSH from bastion server"
+ 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+ 
+    security_groups = [data.aws_security_group.bastion_sg.id]
+  }
+ 
+  ingress {
+    from_port   = 514
+    to_port     = 514
+    protocol    = "tcp"
+ 
+    security_groups = [data.aws_security_group.bastion_sg.id]
+  }
+
+  tags = {
+    Name 	= "platform-docker"
+    Environment = var.env
+  }
+}
+ 
+resource "aws_instance" "docker" {
+  ami 		= "ami-0e38b48473ea57778"
+  instance_type = "t2.medium"
+  key_name 	= "aws-ec2-user"
+  subnet_id 	= data.aws_subnet.subnet_pri_1.id
+  security_groups = [aws_security_group.docker_sg.id]
+  private_ip	= var.private_ip 
+ 
+  tags = {
+    Name 	= "platform-docker"
+    HostType 	= "docker"
+    Environment = var.env
+  }
+}
---
+++ aws/docker/variables.tf
@@ -0,0 +1,19 @@
+#
+# AWS Region
+#
+variable "region" {
+}
+
+#
+# Specifies the environment
+#
+variable "env" {
+}
+
+#
+# Network
+#
+
+variable "private_ip" {
+  default	= "10.0.2.30"
+}
