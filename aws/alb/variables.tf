fee15f9c3c0b675f360cc7f3bc653b8ff0de832c
Dan Kelly <dan@boxcarapp.com>
2020-05-11 11:09:16 -0400

    Updates for LB output of public DNS name and for setting the regional owner of the LB

--- aws/alb/variables.tf
+++ aws/alb/variables.tf
@@ -1,6 +1,11 @@
 variable "region" {}
 variable "env" {}
 variable "key_pair_name" {}
+
+/*
+ * This is the Elastic Load Balancing Account ID for the region (us-east-2).  Update
+ * it for your region
+ */
 variable "alb_account" {
     default = "033677994240"
 }
