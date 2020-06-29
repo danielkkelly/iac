730ecfecd15987081a066fe11409dbca36be4932
Dan Kelly <dan@boxcarapp.com>
2020-06-29 17:59:09 -0400

    Move outputs to a separate file

---
+++ aws/iam/outputs.tf
@@ -0,0 +1,7 @@
+output "user_key_id" {
+  value = [for access_key in aws_iam_access_key.user_access_key: {
+     "user"                        = access_key.user,
+     "access_key_id"               = access_key.id,
+     "access_key_encrypted_secret" = access_key.encrypted_secret
+  }]
+}
