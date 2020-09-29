757ad1e0478d9b89ce578eb0810f2cd19885f62b
Dan Kelly <dan@boxcarapp.com>
2020-09-29 18:13:22 -0400

    Adds a method to pull terraform state

---
+++ script/common-tf.sh
@@ -0,0 +1,8 @@
+function get_terraform_output_value {
+    local provider=$1
+	local module=$2
+	local output_name=$3
+	local output_name_value=$(tfctl.sh --provider $provider --module $module --env $env --action output)
+	local output_value=$(echo "$output_name_value" | sed -n "s/^$output_name = //p")
+	echo $output_value
+}
