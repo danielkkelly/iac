5aa4de844b63fce2c781e025c81f1a4731310a66
Dan Kelly <dan@boxcarapp.com>
2021-01-14 17:37:18 -0500

    Creates distinct eks modules for fargage and managed node groups

---
+++ aws/eks-fargate/variables.tf
@@ -0,0 +1,30 @@
+# AWS
+variable "region" {}
+variable "key_pair_name" {}
+
+# Environment
+variable "env" {}
+
+# EKS
+variable "eks_cluster_name" {
+  default = "platform-eks"
+}
+
+#
+# Network
+#
+variable "host_number" {
+  default = "20"
+}
+
+# Variables that are passed to the eks module
+variable cidr_block_subnet_pri_1 {}
+variable cidr_block_subnet_pri_2 {}
+
+# To silence TF warnings
+variable cidr_block_vpc {}
+variable cidr_block_subnet_pub_1 {}
+variable cidr_block_subnet_pub_2 {}
+variable cidr_block_subnet_rds_1 {}
+variable cidr_block_subnet_rds_2 {}
+variable cidr_block_subnet_vpn_1 {}
