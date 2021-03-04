2ae30d7166116073a04334fdd60e638aaca4b048
Dan Kelly <dan@boxcarapp.com>
2021-03-04 16:14:14 -0500

    Moves S3 resources to dedicated file

---
+++ aws/security/config-s3.tf
@@ -0,0 +1,77 @@
+
+module "config_s3_bucket" {
+  source = "../secure-s3-bucket"
+  name   = "config"
+  env    = var.env
+}
+
+resource "aws_s3_bucket_policy" "config_bucket_policy" {
+  bucket = module.config_s3_bucket.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Allow bucket ACL check",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+         "config.amazonaws.com"
+        ]
+      },
+      "Action": "s3:GetBucketAcl",
+      "Resource": "${module.config_s3_bucket.arn}",
+      "Condition": {
+        "Bool": {
+          "aws:SecureTransport": "true"
+        }
+      }
+    },
+    {
+      "Sid": "Allow bucket write",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+         "config.amazonaws.com"
+        ]
+      },
+      "Action": "s3:PutObject",
+      "Resource": "${module.config_s3_bucket.arn}/*",
+      "Condition": {
+        "StringEquals": {
+          "s3:x-amz-acl": "bucket-owner-full-control"
+        },
+        "Bool": {
+          "aws:SecureTransport": "true"
+        }
+      }
+    },
+    {
+      "Sid": "Require SSL",
+      "Effect": "Deny",
+      "Principal": "*",
+      "Action": "*",
+      "Resource": "${module.config_s3_bucket.arn}/*",
+      "Condition": {
+        "Bool": {
+          "aws:SecureTransport": "false"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
+
+# Needs to be here due to dependency on the policy, otherwise could modularize
+resource "aws_s3_bucket_public_access_block" "s3_bucket_pab" {
+  bucket = module.config_s3_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  depends_on = [aws_s3_bucket_policy.config_bucket_policy]
+}
--- aws/security/config.tf
+++ aws/security/config.tf
@@ -23,87 +23,6 @@ resource "aws_iam_role_policy_attachment" "config_role_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
 }
 
-resource "random_id" "random_s3_index" {
-  byte_length = 4
-}
-
-resource "aws_s3_bucket" "config_s3_bucket" {
-  bucket        = "platform-config-${var.env}-${random_id.random_s3_index.hex}"
-  acl           = "private"
-  force_destroy = true
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "config_bucket_policy" {
-  bucket = aws_s3_bucket.config_s3_bucket.id
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "Allow bucket ACL check",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": [
-         "config.amazonaws.com"
-        ]
-      },
-      "Action": "s3:GetBucketAcl",
-      "Resource": "${aws_s3_bucket.config_s3_bucket.arn}",
-      "Condition": {
-        "Bool": {
-          "aws:SecureTransport": "true"
-        }
-      }
-    },
-    {
-      "Sid": "Allow bucket write",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": [
-         "config.amazonaws.com"
-        ]
-      },
-      "Action": "s3:PutObject",
-      "Resource": "${aws_s3_bucket.config_s3_bucket.arn}/*",
-      "Condition": {
-        "StringEquals": {
-          "s3:x-amz-acl": "bucket-owner-full-control"
-        },
-        "Bool": {
-          "aws:SecureTransport": "true"
-        }
-      }
-    },
-    {
-      "Sid": "Require SSL",
-      "Effect": "Deny",
-      "Principal": "*",
-      "Action": "*",
-      "Resource": "${aws_s3_bucket.config_s3_bucket.arn}/*",
-      "Condition": {
-        "Bool": {
-          "aws:SecureTransport": "false"
-        }
-      }
-    }
-  ]
-}
-POLICY
-}
-
 resource "aws_config_configuration_recorder" "config_recorder" {
   name     = "platform-config-recorder"
   role_arn = aws_iam_role.config_role.arn
@@ -120,7 +39,9 @@ module "default_sns" {
   monthly_spend_limit    = 1
   topic_name             = "platform-sms-topic"
   topic_display_name     = "platform-sms-topic"
-  usage_report_s3_bucket = "platform-sms-usage-bucket-${var.env}"
+  usage_report_s3_bucket = "sms-usage"
+
+  env = var.env
 
   subscriptions = [
     var.sms_number
@@ -129,9 +50,9 @@ module "default_sns" {
 
 resource "aws_config_delivery_channel" "config_delivery_channel" {
   name           = "platform-config-delivery-channel"
-  s3_bucket_name = aws_s3_bucket.config_s3_bucket.bucket
-  sns_topic_arn = module.default_sns.topic_arn
-  depends_on = [aws_config_configuration_recorder.config_recorder]
+  s3_bucket_name = module.config_s3_bucket.bucket
+  sns_topic_arn  = module.default_sns.topic_arn
+  depends_on     = [aws_config_configuration_recorder.config_recorder]
 }
 
 resource "aws_config_configuration_recorder_status" "config_recorder_status" {
