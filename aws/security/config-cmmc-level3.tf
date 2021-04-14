e30b576ac870c37eccd8a7864dced9e8e928a869
Dan Kelly <dan@boxcarapp.com>
2021-04-14 16:26:31 -0400

    Adds config conformance pack for CMMC level 3

---
+++ aws/security/config-cmmc-level3.tf
@@ -0,0 +1,10 @@
+resource "aws_config_conformance_pack" "cmmc_level3_conformance_pack" {
+  name = "operational-best-practices-for-cmmc-level3"
+  template_body = data.http.conformance_pack.body
+
+  depends_on = [aws_config_configuration_recorder.config_recorder]
+}
+
+data "http" "conformance_pack" {
+  url = "https://raw.githubusercontent.com/awslabs/aws-config-rules/master/aws-config-conformance-packs/Operational-Best-Practices-for-CMMC-Level-3.yaml"
+}
