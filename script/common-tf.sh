1a618d030a2143d620c037f2c2e9326deef96fb1
Dan Kelly <dan@boxcarapp.com>
2021-07-11 13:13:34 -0400

    Fixes a but in parsing outputs

--- script/common-tf.sh
+++ script/common-tf.sh
@@ -3,6 +3,6 @@ function get_terraform_output_value {
 	local module=$2
 	local output_name=$3
 	local output_name_value=$(tfctl.sh --provider $provider --module $module --env $env --action output)
-	local output_value=$(echo "$output_name_value" | sed -n "s/^$output_name = //p")
+	local output_value=$(echo "$output_name_value" | sed -n "s/^$output_name = //p" | sed 's/"//g')
 	echo $output_value
 }
