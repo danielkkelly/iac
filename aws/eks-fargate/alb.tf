c4776361cf093ae0f8105f9030fcf3f8a24d1677
Dan Kelly <dan@boxcarapp.com>
2021-07-28 17:59:19 -0400

    Adds listner rules and target group as well as security group rules to allow the load balancer access to cluster resources

--- aws/eks-fargate/alb.tf
+++ aws/eks-fargate/alb.tf
@@ -14,7 +14,42 @@ data "aws_lb_listener" "lb_listener_https" {
  */
 resource "aws_lb_target_group" "platform_pod_lb_tg" {
   name     = "platform-pod"
-  port     = 8080
+  port     = 80
   protocol = "HTTP"
+  target_type = "ip"
   vpc_id   = data.aws_vpc.vpc.id
+}
+
+resource "aws_lb_listener_rule" "pod_listener_rule" {
+  listener_arn = data.aws_lb_listener.lb_listener_https.arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.platform_pod_lb_tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
+}
+
+/* 
+ * Security group updates to allow the load balancer access to the instances
+ */
+data "aws_security_group" "lb_sg" {
+  tags = {
+    Name = "platform-lb"
+  }
+}
+
+resource "aws_security_group_rule" "lb_http_sgr" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = data.aws_security_group.lb_sg.id
+  security_group_id        = module.eks.cluster_primary_security_group_id
 }
