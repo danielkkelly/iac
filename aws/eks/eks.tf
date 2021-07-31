1c48a35d6698e704901687cc419119d8d3dd5dfc
Dan Kelly <dan@boxcarapp.com>
2021-07-31 14:17:55 -0400

    Renames eks-fargate to eks and moves managed node groups to it to consolidate

--- aws/eks/eks.tf
+++ aws/eks/eks.tf
@@ -36,20 +36,47 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "~> 1.11"
 }
 
 module "eks" {
-  source       = "terraform-aws-modules/eks/aws"
-  cluster_name = var.eks_cluster_name
-  subnets      = [for s in data.aws_subnet.subnet_id : s.id]
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.eks_cluster_name
+  cluster_version = var.eks_cluster_version
+  subnets         = [for s in data.aws_subnet.subnet_id : s.id]
+
+  # Creates the OIDC provider
+  enable_irsa = true
+
+  # We use the AWS command line to update kubeconfig based on cluster name
+  write_kubeconfig = false
 
   tags = {
     Environment = var.env
   }
 
   vpc_id = data.aws_vpc.vpc.id
+
+  fargate_profiles = {
+    platform = {
+      selectors = [
+        {
+          namespace = "kube-system"
+        },
+        {
+          namespace = "default"
+        },
+        {
+          namespace = "cert-manager"
+        }
+      ]
+
+      tags = {
+        Environment = var.env
+      }
+    }
+  }
+
+  /* Uncomment if you want managed node groups (EC2 instances managed by AWS)
 
   node_groups_defaults = {
     ami_type  = "AL2_x86_64"
@@ -65,9 +92,22 @@ module "eks" {
       instance_type = "t2.medium"
       k8s_labels = {
         Environment = var.env
-        GithubRepo  = "terraform-aws-eks"
-        GithubOrg   = "terraform-aws-modules"
       }
+    }
+  }
+  */
+}
+
+resource "kubernetes_service_account" "lbc_service_account" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    labels = {
+      "app.kubernetes.io/component" = "controller"
+      "app.kubernetes.io/name"      = "aws-load-balancer-controller"
+    }
+    annotations = {
+      "eks.amazonaws.com/role-arn" = "${aws_iam_role.lbc_iam_role.arn}"
     }
   }
 }
