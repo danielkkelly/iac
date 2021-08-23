83d8ebfbd382873bc8fd7e17120daec26cf5859a
Dan Kelly <dan@boxcarapp.com>
2021-08-23 18:22:51 -0400

    Updates formatting

--- aws/rds-mysql/cloudwatch.tf
+++ aws/rds-mysql/cloudwatch.tf
@@ -1,23 +1,23 @@
 locals {
-    log_path = "/aws/rds/instance/${var.rds_cluster_identifier}"
+  log_path = "/aws/rds/instance/${var.rds_cluster_identifier}"
 }
 
 resource "aws_cloudwatch_log_group" "audit" {
-  name = "${local.log_path}/audit"
+  name              = "${local.log_path}/audit"
   retention_in_days = var.cloudwatch_retention_in_days
 }
 
 resource "aws_cloudwatch_log_group" "error" {
-  name = "${local.log_path}/error"
+  name              = "${local.log_path}/error"
   retention_in_days = var.cloudwatch_retention_in_days
 }
 
 resource "aws_cloudwatch_log_group" "general" {
-  name = "${local.log_path}/general"
+  name              = "${local.log_path}/general"
   retention_in_days = var.cloudwatch_retention_in_days
 }
 
 resource "aws_cloudwatch_log_group" "slowquery" {
-  name = "${local.log_path}/slowquery"
+  name              = "${local.log_path}/slowquery"
   retention_in_days = var.cloudwatch_retention_in_days
 }
