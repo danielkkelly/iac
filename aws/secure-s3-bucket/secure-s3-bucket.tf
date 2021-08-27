ba7f19e33f02c15faa971378373d207ab9384deb
Dan Kelly <dan@boxcarapp.com>
2021-08-27 19:20:17 -0400

    Adds a logging bucket for each S3 bucket created

--- aws/secure-s3-bucket/secure-s3-bucket.tf
+++ aws/secure-s3-bucket/secure-s3-bucket.tf
@@ -1,3 +1,7 @@
+locals {
+  base_bucket_name = "platform-${var.name}-${var.env}-${random_id.random_s3_index.hex}"
+}
+
 /* 
  * Sets up for a secure S3 bucket.  It would be even better if we coudl add a stanza to 
  * a policy to deny all without SSL and to handle the aws_s3_bucket_public_access_block
@@ -7,15 +11,31 @@ resource "random_id" "random_s3_index" {
   byte_length = 4
 }
 
+/* 
+ * Shows the FedRAMP controls associated with the configuration.  Note that the
+ * AC-3, AC-4, AC-5, AC-6, AC-14 compliance is enforeced in policy.  These 
+ * policies are specified in the caller of this module but should require SSL
+ * in all cases.  Audited via Security Hub and AWS Config CMMC3 conformance pack.
+ */
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket        = "platform-${var.name}-${var.env}-${random_id.random_s3_index.hex}"
+  bucket        = local.base_bucket_name
   acl           = "private"
   force_destroy = true
 
+  // CM-2, SI-7
   versioning {
     enabled = var.versioning_enabled
   }
 
+  // AU-2, AU-3, AU-9, AU-11
+  logging {
+    target_bucket = aws_s3_bucket.s3_logging_bucket.id
+  }
+
+  // AU-9, CP-6
+  // replication_configuration {}
+
+  // SC-13, SC-28
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
@@ -23,4 +43,61 @@ resource "aws_s3_bucket" "s3_bucket" {
       }
     }
   }
+
+  // SI-12
+  lifecycle_rule { 
+    enabled = true
+
+    transition {
+      days = 90
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 365
+    }
+  }
+}
+
+resource "aws_s3_bucket" "s3_logging_bucket" {
+  bucket        = "${local.base_bucket_name}-logging"
+  acl           = "log-delivery-write"
+  force_destroy = true
+
+  // CM-2, SI-7
+  versioning {
+    enabled = var.versioning_enabled
+  }
+
+  // SC-13, SC-28
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  // SI-12
+  lifecycle_rule { 
+    enabled = true
+
+    transition {
+      days = 90
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 365
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_logging_bucket_pab" {
+  bucket = aws_s3_bucket.s3_logging_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
