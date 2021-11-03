b179530217ea5a679e736366d2b509a06851cd9a
Dan Kelly <dan@boxcarapp.com>
2021-11-03 16:25:31 -0400

    Updates settings to pass providers down to child and grandchild modules

--- aws/settings-sms/settings-sms.tf
+++ aws/settings-sms/settings-sms.tf
@@ -1,8 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.7.0"
+      configuration_aliases = [ aws.default, aws.replica ]
+    }
+  }
+}
+
 data "aws_iam_role" "delivery_status_role" {
+  provider = aws.default
   name = "platform-${var.env}-sns-cloudwatch-logs-role"
 }
 
 resource "aws_sns_sms_preferences" "sms_preferences" {
+  provider = aws.default
   default_sender_id                     = var.default_sender_id
   default_sms_type                      = var.default_sms_type
   delivery_status_iam_role_arn          = data.aws_iam_role.delivery_status_role.arn
