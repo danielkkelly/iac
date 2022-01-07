f6f458b16c1cb39c125229c05f7ccd5015e25c00
Dan Kelly <dan@boxcarapp.com>
2022-01-07 15:41:41 -0500

    Adds a Name tag to KMS key

--- aws/kms-key/kms-key.tf
+++ aws/kms-key/kms-key.tf
@@ -1,6 +1,10 @@
 resource "aws_kms_key" "kms_key" {
   description         = "platform-${var.name}"
   enable_key_rotation = true
+
+  tags = {
+    Name = "platform-${var.name}"
+  }
 }
 
 resource "aws_kms_alias" "kms_alias" {
