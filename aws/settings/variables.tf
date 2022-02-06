9da28da0efc9bbe722fae9078b9db152fbb9e4e0
Dan Kelly <dan@boxcarapp.com>
2022-02-06 14:32:31 -0500

    Improves description of variables

--- aws/settings/variables.tf
+++ aws/settings/variables.tf
@@ -2,7 +2,10 @@
 variable "region" {}
 variable "env" {}
 variable "key_pair_name" {}
-variable "replication_region" {}
+
+variable "replication_region" {
+  description = "Region into which we replicate backups, set globally via environment variable"
+}
 
 # SMS
 variable "sms_enabled" {
